dc8f114c420e7a3bcee82c4cef44b2bfdfd376e1
Keith Leonardo <kleonardo52@gmail.com>
2019-12-04 13:58:14 +0800

    Make Actor direction_ protected member
    Add accessor functions to the interface instead.

--- src/actor.cpp
+++ src/actor.cpp
@@ -3,6 +3,12 @@
 namespace Dynamo {
     Actor::Actor(AABB hitbox, bool solid) {
         alive_ = true;
+        
+        direction_ = {
+            NO_DIRECTION,
+            NO_DIRECTION
+        };
+
         body_ = {
             hitbox,
             {0, 0},
@@ -10,14 +16,19 @@ namespace Dynamo {
             false,
             solid
         };
-        direction_ = {
-            NO_DIRECTION,
-            NO_DIRECTION
-        };
     }
 
     bool Actor::is_alive() {
         return alive_;
+    }
+
+    ACTOR_DIRECTION Actor::get_direction() {
+        return direction_.current;
+    }
+
+    void Actor::set_direction(ACTOR_DIRECTION direction) {
+        direction_.prev = direction_.current;
+        direction_.current = direction;
     }
 
     void Actor::kill() {
--- src/actor.h
+++ src/actor.h
@@ -36,14 +36,22 @@ namespace Dynamo {
     class Actor {
         bool alive_;
 
+    protected:
+        Direction direction_;
+
     public:
         RigidBody body_;
-        Direction direction_;
 
         Actor(AABB hitbox, bool solid);
 
         // Check if the actor is alive
         bool is_alive();
+
+        // Get the current direction of the actor
+        ACTOR_DIRECTION get_direction();
+
+        // Set the current direction of the actor
+        void set_direction(ACTOR_DIRECTION direction);
 
         // Kill the actor
         void kill();
