c43827bea8767026ce3b6fd24a72c4f2dc69e591
Keith Leonardo <kleonardo52@gmail.com>
2020-02-04 20:36:53 +1100

    Add specifications of revamped Jukebox

--- src/core/jukebox.h
+++ src/core/jukebox.h
@@ -9,6 +9,33 @@
 #include <string>
 
 namespace Dynamo {
+    /** TODO: Jukebox Engine Specs
+     *
+     * Two types of sound
+     *      Bite (Decoded once, stored in memory)
+     *      Stream (Decoded on request from audio device)
+     * Objects:
+     *      Sound (Stores the file and encoded data)
+     *      Jukebox (Main engine class)
+     * Methods:
+     *      int get_stream()
+     *      int generate_stream()
+     *
+     *      Bite *load_bite(std::string filename)
+     *      void play_bite(Bite *load_bite, float volume)
+     *      
+     *      void stream(std::string filename, float volume, int stream_id)
+     *      void play_stream(int track_id)
+     *      void pause_stream(int track_id)
+     *      void stop_stream(int track_id)
+     *
+     *      void set_volume(float volume)
+     *
+     *      void stop()
+     *      void play()
+     *
+     *      void update()
+     */
     class Jukebox {
         // Unfortunately, SDL_Mixer does not allow multiple
         // concurrent music streams for ambient noise
