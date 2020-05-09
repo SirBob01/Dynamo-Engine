4536098668a3d13f22529ee6869eab45485ad133
Keith Leonardo <kleonardo52@gmail.com>
2020-05-09 14:20:36 +1000

    Make Dynamo::Util a namespace instead of a class with static methods.

--- src/util/util.h
+++ src/util/util.h
@@ -3,19 +3,16 @@
 
 #include <algorithm>
 
-namespace Dynamo {
-    class Util {
-    public:
-        template <typename T, typename R>
-        static T lerp(T v0, T v1, R t) {
-            return (1 - t) * v0 + t * v1;
-        }
+namespace Dynamo::Util {
+    template <typename T, typename R>
+    T lerp(T v0, T v1, R t) {
+        return (1 - t) * v0 + t * v1;
+    }
 
-        template <typename T>
-        static T clamp(T x, T min, T max) {
-            return std::min(max, std::max(x, min));
-        }
-    };
+    template <typename T>
+    T clamp(T x, T min, T max) {
+        return std::min(max, std::max(x, min));
+    }
 }
 
 #endif
