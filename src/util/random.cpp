d5a61db6cdd47065d5b945b82b2775a57bc5361f
Keith Leonardo <kleonardo52@gmail.com>
2020-05-09 14:17:22 +1000

    Make Dynamo::Random a namespace instead of a class with static methods

--- src/util/random.cpp
+++ src/util/random.cpp
@@ -1,15 +1,15 @@
 #include "random.h"
 
-namespace Dynamo {
-    void Random::seed(int s) {
+namespace Dynamo::Random {
+    void seed(int s) {
         std::srand(s);
     }
 
-    float Random::random() {
+    float random() {
         return static_cast<float>(std::rand()) / RAND_MAX;
     }
 
-    float Random::range(float min, float max) {
+    float range(float min, float max) {
         return min + ((max - min) * random());
     }
 }
--- src/util/random.h
+++ src/util/random.h
@@ -3,18 +3,15 @@
 
 #include <cstdlib>
 
-namespace Dynamo {
-    class Random {
-    public:
-        // Seed the random number generator
-        static void seed(int s);
+namespace Dynamo::Random {
+    // Seed the random number generator
+    void seed(int s);
 
-        // Get a random number between 0 and 1
-        static float random();
+    // Get a random number between 0 and 1
+    float random();
 
-        // Get a random number between min and max
-        static float range(float min, float max);
-    };
+    // Get a random number between min and max
+    float range(float min, float max);
 }
 
 #endif
