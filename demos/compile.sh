5d8ebf258f4bda1bf613496468fe0e0813323ff3
Keith Leonardo <kleonardo52@gmail.com>
2020-05-21 12:54:07 +1000

    Remove compile.sh for demos

--- demos/compile.sh
+++
@@ -1,9 +0,0 @@
-# Automatically compile all demo programs
-SRC_FILES="$(find ../src -name '*.cpp')";
-
-shopt -s globstar
-for i in **/*.cpp;
-do
-    echo "Compiling $i...";
-    g++ -std=c++17 -g -O $i $SRC_FILES -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_net -lvorbis -lvorbisfile -o "${i%.cpp}";
-done
