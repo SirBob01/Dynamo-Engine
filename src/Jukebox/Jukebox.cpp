6e9f6d480df250933578f8fdaa3f2202433c0b7d
Keith Leonardo <kleonardo52@gmail.com>
2022-10-17 23:22:10 +1100

    Install PortAudio library for cross-platform audio playback and recording

---
+++ src/Jukebox/Jukebox.cpp
@@ -0,0 +1,3 @@
+#include "Jukebox.hpp"
+
+namespace Dynamo {}
---
+++ src/Jukebox/Jukebox.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <queue>
+#include <string>
+#include <vector>
+
+#include <portaudio.h>
+#include <stb_vorbis.c>
+
+namespace Dynamo {} // namespace Dynamo
