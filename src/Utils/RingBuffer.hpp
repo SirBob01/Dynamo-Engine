921e4bc40a4ebb9424b169fbc9d222b74403621c
Keith Leonardo <kleonardo52@gmail.com>
2022-12-16 00:59:07 +1100

    Implement buffer copy RingBuffer read and write

--- src/Utils/RingBuffer.hpp
+++ src/Utils/RingBuffer.hpp
@@ -11,7 +11,7 @@ namespace Dynamo {
      * The buffer is full if write is just behind read
      * The buffer is empty if the read and write point to the same index
      *
-     * @tparam T Type of element
+     * @tparam T Type of element, must be trivially copyable
      * @tparam N Maximum size of the container (power of 2)
      */
     template <typename T, unsigned N>
@@ -19,6 +19,8 @@ namespace Dynamo {
         static const unsigned MASK = N - 1;
         static_assert(N > 0 && (N & MASK) == 0,
                       "RingBuffer size (> 0) should be a power of 2");
+        static_assert(std::is_trivially_copyable<T>::value,
+                      "RingBuffer element type must be trivially copyable");
 
         std::array<T, N> _buffer;
         unsigned _read;
@@ -79,6 +81,57 @@ namespace Dynamo {
         inline void write(T value) {
             DYN_ASSERT(!is_full());
             _buffer[_write++ & MASK] = value;
+        }
+
+        /**
+         * @brief Read up to n values from the buffer, advancing the read
+         * pointer
+         *
+         * This will return the number of elements read
+         *
+         * @param dst
+         * @param n
+         * @return unsigned
+         */
+        inline unsigned read(T *dst, unsigned n) {
+            // Compute copy partitions
+            unsigned offset = _read & MASK;
+            unsigned length = std::min(n, size());
+            unsigned l_length = std::min(length, N - offset);
+            unsigned r_length = length - l_length;
+
+            T *src = _buffer.data();
+            std::copy(src + offset, src + offset + l_length, dst);
+            std::copy(src, src + r_length, dst + l_length);
+
+            // Update read pointer
+            _read += length;
+            return length;
+        }
+
+        /**
+         * @brief Write up to n values into the buffer, advancing the write
+         * pointer
+         *
+         * This will return the number of elements written
+         *
+         * @param src
+         * @param n
+         * @return unsigned
+         */
+        inline unsigned write(T *src, unsigned n) {
+            // Compute copy partitions
+            unsigned offset = _write & MASK;
+            unsigned length = std::min(n, remaining());
+            unsigned l_length = std::min(length, N - offset);
+
+            T *dst = _buffer.data();
+            std::copy(src, src + l_length, dst + offset);
+            std::copy(src + l_length, src + length, dst);
+
+            // Update write pointer
+            _write += length;
+            return length;
         }
 
         /**
--- tests/src/RingBuffer-Test.cpp
+++ tests/src/RingBuffer-Test.cpp
@@ -53,6 +53,87 @@ TEST_CASE("RingBuffer read", "[RingBuffer]") {
     CHECK_THROWS(buffer.read());
 }
 
+TEST_CASE("RingBuffer write buffer", "[RingBuffer]") {
+    Dynamo::RingBuffer<short, 4> buffer;
+    short x[6] = {1, 2, 3, 4, 5, 6};
+
+    REQUIRE(buffer.size() == 0);
+    REQUIRE(buffer.remaining() == 4);
+
+    REQUIRE(buffer.write(x, 1) == 1);
+    REQUIRE(buffer.size() == 1);
+    REQUIRE(buffer.remaining() == 3);
+
+    REQUIRE(buffer.write(x + 1, 2) == 2);
+    REQUIRE(buffer.size() == 3);
+    REQUIRE(buffer.remaining() == 1);
+
+    REQUIRE(buffer.write(x + 3, 3) == 1);
+    REQUIRE(buffer.size() == 4);
+    REQUIRE(buffer.remaining() == 0);
+    REQUIRE(buffer.is_full());
+
+    CHECK_THROWS(buffer.write(5));
+    for (int i = 0; i < 4; i++) {
+        REQUIRE(buffer.read() == i + 1);
+    }
+}
+
+TEST_CASE("RingBuffer read buffer", "[RingBuffer]") {
+    Dynamo::RingBuffer<short, 4> buffer;
+    short x[4] = {1, 2, 3, 4};
+    short y[4] = {0, 0, 0, 0};
+    buffer.write(x, 4);
+
+    REQUIRE(buffer.read(y, 1) == 1);
+    REQUIRE(y[0] == 1);
+    REQUIRE(buffer.size() == 3);
+    REQUIRE(buffer.remaining() == 1);
+
+    REQUIRE(buffer.read(y + 1, 2) == 2);
+    REQUIRE(y[1] == 2);
+    REQUIRE(y[2] == 3);
+    REQUIRE(buffer.size() == 1);
+    REQUIRE(buffer.remaining() == 3);
+
+    REQUIRE(buffer.read(y + 3, 1) == 1);
+    REQUIRE(y[3] == 4);
+    REQUIRE(buffer.size() == 0);
+    REQUIRE(buffer.remaining() == 4);
+    REQUIRE(buffer.is_empty());
+
+    CHECK_THROWS(buffer.read());
+    for (int i = 0; i < 4; i++) {
+        REQUIRE(y[i] == i + 1);
+    }
+}
+
+TEST_CASE("RingBuffer overlapping read write", "[RingBuffer]") {
+    Dynamo::RingBuffer<short, 4> buffer;
+    short x[6] = {1, 2, 3, 4, 5, 6};
+    short y[6] = {0, 0, 0, 0, 0, 0};
+
+    REQUIRE(buffer.write(x, 2) == 2);
+    REQUIRE(buffer.size() == 2);
+    REQUIRE(buffer.remaining() == 2);
+
+    REQUIRE(buffer.read(y, 2) == 2);
+    REQUIRE(y[0] == 1);
+    REQUIRE(y[1] == 2);
+    REQUIRE(buffer.size() == 0);
+    REQUIRE(buffer.remaining() == 4);
+
+    REQUIRE(buffer.write(x + 2, 3) == 3);
+    REQUIRE(buffer.size() == 3);
+    REQUIRE(buffer.remaining() == 1);
+
+    REQUIRE(buffer.read(y + 2, 4) == 3);
+    REQUIRE(y[2] == 3);
+    REQUIRE(y[3] == 4);
+    REQUIRE(buffer.size() == 0);
+    REQUIRE(buffer.remaining() == 4);
+}
+
 TEST_CASE("RingBuffer pop", "[RingBuffer]") {
     Dynamo::RingBuffer<short, 4> buffer;
     buffer.write(1);
